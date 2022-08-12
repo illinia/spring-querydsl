1bf856d571147bf37f8ad8b9eec24c345b3fc832
김태민 <seolin1218@gmail.com>
2022-08-12 16:41:55 +0300

    프로젝션 튜플

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -431,4 +431,30 @@ public class QuerydslBasicTest {
             System.out.println("s = " + s);
         }
     }
+    
+    @Test
+    public void simpleProjection() throws Exception {
+        List<Member> result = queryFactory
+                .select(member)
+                .from(member)
+                .fetch();
+        for (Member s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void tupleProjection() throws Exception {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
 }
