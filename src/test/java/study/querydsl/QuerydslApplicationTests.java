6079d3481edb99cc6522945e6fbc8b4eb0579364
김태민 <seolin1218@gmail.com>
2022-08-12 11:38:07 +0300

    프로젝트 시작, querydsl 세팅, 테스트

---
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,42 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	@Autowired
+	EntityManager em;
+
+	@Test
+	public void test() throws Exception {
+	    //given
+		Hello hello = new Hello();
+		em.persist(hello);
+		System.out.println("hello = " + hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = query.selectFrom(qHello)
+				.fetchOne();
+		System.out.println("result = " + result);
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+
+	}
+
+}
